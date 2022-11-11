ac5189353e9b9f3eced5fdea7950dc77b7a3a80d
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-11-11 11:09:35 +0100

    Add GithubApiUserData class that handles requests to GitHub API

---
+++ app/src/main/java/com/example/github_api_handler/GithubUserApiData.java
@@ -0,0 +1,73 @@
+package com.example.github_api_handler;
+
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ */
+public class GithubUserApiData {
+
+    /**
+     *
+     */
+    private final String userUrlAPI;
+
+    /**
+     *
+     */
+    private JSONObject userData;
+
+    /**
+     *
+     *
+     * @param nickname
+     * @throws Exception
+     */
+    public GithubUserApiData(String nickname) throws Exception {
+        this.userUrlAPI = "https://api.github.com/users/" + nickname;
+
+        this.fetchData();
+    }
+
+    /**
+     *
+     *
+     * @throws Exception
+     */
+    public void fetchData() throws Exception {
+        URL obj = new URL(this.userUrlAPI);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = input.readLine()) != null) {
+            response.append(inputLine);
+        }
+
+        input.close();
+
+        this.userData = new JSONObject(response.toString());
+    }
+
+    /**
+     *
+     *
+     * @param element
+     * @return String
+     * @throws JSONException
+     */
+    public String get(String element) throws JSONException {
+        return this.userData.getString(element);
+    }
+}
--- app/src/main/java/com/example/github_api_handler/MainActivity.java
+++ app/src/main/java/com/example/github_api_handler/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.github_api_handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.StrictMode;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View rootView = findViewById(android.R.id.content);
+
+        assignEvents(rootView);
+
+
     }
+
+    /**
+     *
+     */
+    protected void assignEvents(View view) {
+        Button searchBtn = (Button) findViewById(R.id.searchBtn);
+
+        searchBtn.setOnClickListener(event -> tryToFindUser(view));
+    }
+
+    /**
+     *
+     */
+    protected void tryToFindUser(View view) {
+        TextView infoTextView = (TextView) findViewById(R.id.infoTextView);
+        EditText nicknameText = (EditText) findViewById(R.id.nicknameText);
+        String nickname = nicknameText.getText().toString();
+        if (nickname.length() == 0) {
+            infoTextView.setText("");
+            return;
+        }
+
+        RelativeLayout loadingPanel = (RelativeLayout) findViewById(R.id.loadingPanel);
+        loadingPanel.setVisibility(View.VISIBLE);
+
+        try {
+            GithubUserApiData userData = new GithubUserApiData(nickname);
+            infoTextView.setVisibility(View.GONE);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            infoTextView.setText("An error occurred, please try again later");
+            infoTextView.setVisibility(View.VISIBLE);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            infoTextView.setText("Couldn't find user with passed nickname");
+            infoTextView.setVisibility(View.VISIBLE);
+        }
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            loadingPanel.setVisibility(View.GONE);
+        }, 500);
+    }
+
+
 }
